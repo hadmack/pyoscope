5084da1a188f478cbae3a2b0300642fcb5b07dd8
Mike Hadmack <hadmack@gmail.com>
2011-02-02 21:59:34 -1000

    Added udev config instructions to README Added test main() function to instrument.py Added interface methods for many of the Rigol Scope calls in capture_channel_1.py Started work on getWaveform() This commit has not been tested!

--- instrument.py
+++ instrument.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import os
 import sys
+import numpy
 
 class usbtmc:
     """Simple implementation of a USBTMC device driver, in the style of visa.h"""
@@ -45,6 +47,7 @@ class usbtmc:
         os.close(self.FILE)
 
 
+WAV_PREAMBLE_LENGTH = 10
 
 class RigolScope(usbtmc):
     """Class to control a Rigol DS1000 series oscilloscope"""
@@ -52,20 +55,64 @@ class RigolScope(usbtmc):
         usbtmc.__init__(self, device)
         self.name = self.getName()
         print "Connected to: " + self.name
-
+    
     def stop(self):
         """Stop acquisition"""
         self.write(":STOP")
-
+    
+    def run(self):
+        """Start acquisition"""
+        self.write(":RUN")
+    
+    def forceTrigger(self):
+        """Force the scope to trigger now"""
+        self.write(":KEY:FORC")
+    
     def unlock(self):
         """Unlock scope panel keys"""
         self.write(":KEY:LOCK DIS")
-
+    
     def close(self):
         """Overload usbtmc close for Rigol specific commands"""
         self.unlock()
         usbtmc.close(self)
-
+    
+    def readRawData(self,chan=1):
+        """Read raw data from scope channel"""
+        command = ":WAV:DATA? CHAN" + str(chan)
+        self.write(command)
+        return self.read(9000)
+    
+    def readData(self,chan=1):
+        """Read scope channel and return numpy array"""
+        rawdata = self.readRawData(chan)
+        return numpy.frombuffer(rawdata, dtype='B', offset=WAV_PREAMBLE_LENGTH)
+    
+    def getVoltScale(self,chan=1):
+        return float(self.query(":CHAN"+str(chan)+":SCAL?", 20))
+    
+    def getVoltOffset(self,chan=1):
+        return float(self.query(":CHAN"+str(chan)+":OFFS?", 20))
+    
+    def getTimeScale(self):
+        return float(self.query(":TIM:SCAL?", 20))
+        
+    def getTimeOffset(self):
+        return float(self.query(":TIM:OFFS?", 20))
+    
+    def getWaveform(self,chan=1):
+        """Read scope and rescale data from axis information
+           return data as a tuple (t,y)
+           TODO: Multichannel mode
+           TODO: Return scope parameters in dictionary """
+        data = self.readData(chan)
+        
+        voltscale  = getVoltScale(chan)
+        voltoffset = getVoltScale(chan)
+        timescale  = getTimeScale()
+        timeoffset = getTimeOffset()
+    
+        
 def main():
     print "# RigolScope Test #"
     scope = RigolScope("/dev/usbtmc0")
