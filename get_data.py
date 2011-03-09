aae050046cd851f8368cdfc0acb0758a5f300356
Mike Hadmack <hadmack@gmail.com>
2011-03-09 11:52:22 -1000

    new interface to dumping to file

---
+++ get_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# PyUSBtmc
+# get_data.py
+#
+# Copyright (c) 2011 Mike Hadmack
+# This code is distributed under the MIT license
+import numpy
+import sys
+from matplotlib import pyplot
+from pyusbtmc import RigolScope
+""" Capture data from Rigol oscilloscope and write to a file 
+    usage: python save_channel.py <filename>
+    if filename is not given STDOUT will be used"""
+
+try:
+    filename = sys.argv[1]
+except:
+    filename = ""
+
+print filename
+#scope = RigolScope("/dev/usbtmc0")
+#scope.grabData()
+#scope.writeWaveformToFile("out.dat")
+#scope.close()
